1cdddb5037cf8d523c4367ea3c712fb8a4ea64a3
Lucina Chavez <lucina.chavez@grupoabsa.com>
2018-11-24 00:17:12 +0000

    [REF] __manifest__.py, controllerspy, models.py

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,52 +1,54 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api,exceptions, _
-import time
-from psycopg2 import IntegrityError
+from odoo import models, fields, api, exceptions, _
+# import time
+#from psycopg2 import IntegrityError
 from datetime import timedelta
 
+
 def get_uid(self, *a):
-   # import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
     return self.env.uid
+
 
 class Course(models.Model):
     _name = 'openacademy.course'
 
-    name = fields.Char(string="Title",required=True)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
     responsible_id = fields.Many2one(
-            'res.users', string="Responsible",
-            index=True,  ondelete='set null',
-         #  default=lambda self, *a: self.env.uid)
-         default=get_uid)
-    session_ids=fields.One2many('openacademy.session','course_id')
+        'res.users', string="Responsible",
+        index=True,  ondelete='set null',
+#  default=lambda self, *a: self.env.uid)
+        default=get_uid)
+    session_ids=fields.One2many('openacademy.session', 'course_id')
 
     _sql_constraints = [
-            ('name_description_check',
-                'CHECK(name != description)',
-                "The title of the course should not be the descripcion"
-            ),
-            ('name_unique',
-                'UNIQUE(name)',
-                "The course title must be unique",
-            ),
-            ]
+        ('name_description_check',
+        'CHECK(name != description)',
+        "The title of the course should not be the descripcion"
+        ),
+        ('name_unique',
+        'UNIQUE(name)',
+        "The course title must be unique",
+        ),
+        ]
 
     def copy(self, default=None):
         print ("estoy pasando por la funcion heredada de copy en cursos")
         if default is None:
             default = {}
         copied_count = self.search_count([
-            ('name','ilike',_('Copy of %s%%') % (self.name))])
+            ('name', 'ilike',_('Copy of %s%%') % (self.name))])
         if not copied_count:
             new_name = _("Copy of %s") % (self.name)
         else:
             new_name =_("Copy of %s (%s)") % (self.name, copied_count)
         default['name'] = new_name
-        #try:
+#try:
         return super(Course, self).copy(default)
-        #except IntegrityError:
-         #   import pdb; pdb.set_trace()
+#except IntegrityError:
+#   import pdb; pdb.set_trace()
 
 
 class Session(models.Model):
@@ -57,11 +59,11 @@ class Session(models.Model):
     datetime_test = fields.Datetime(default=fields.Datetime.now)
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner',string='Instructor',domain=['|',('instructor','=',True),
-        ('category_id.name','ilike','Teacher')])
+    instructor_id = fields.Many2one('res.partner', string='Instructor', domain=['|', ('instructor', '=', True),
+        ('category_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade',
             string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner',string="Attendees")
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(compute='_taken_seats', store = True)
     active = fields.Boolean(default=True)
     end_date = fields.Date(store=True, compute='_get_end_date',
@@ -97,42 +99,40 @@ class Session(models.Model):
             end_date = fields.Date.from_string(record.end_date)
             record.duration = (end_date - start_date).days + 1
 
-    @api.depends('seats','attendee_ids')
+    @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
         for record in self.filtered(lambda r: r.seats != 0):
             record.taken_seats = 100 * len(record.attendee_ids)/record.seats
-      #  import pdb; pdb.set_trace()
-      #  for record in self:
-      #      if not record.seats:
-      #          record.taken_seats = 0
-      #      else:
-      #          record.taken_seats = 100 * len(record.attendee_ids)/record.seats
+#        import pdb; pdb.set_trace()
+#        for record in self:
+#        if not record.seats:
+#            record.taken_seats = 0
+#        else:
+#            record.taken_seats = 100 * len(record.attendee_ids)/record.seats
 
-
-    @api.onchange('seats','attendee_ids')
+    @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
         if self.filtered(lambda r: r.seats < 0):
             self.active = False
             return {
-                    'warning': {
-                        'title': _("Incorrect 'seats' value"),
-                        'message': _("The number of available seats may not be negative"),
-                        }
+                'warning': {
+                    'title': _("Incorrect 'seats' value"),
+                    'message': _("The number of available seats may not be negative"),
                     }
+                }
         if self.seats < len(self.attendee_ids):
             self.active = False
             return {
-                    'warning':{
-                        'title': _("Too many attendees"),
-                        'message': _("Increase seats or remove excess attendees"),
-                        }
+                'warning':{
+                    'title': _("Too many attendees"),
+                    'message': _("Increase seats or remove excess attendees"),
                     }
+                }
         self.active = True
-
 
     @api.constrains('instructor_id','attendee_ids')
     def _check_instructor_not_in_attendees(self):
         for record in self.filtered('instructor_id'):
             if record.instructor_id in record.attendee_ids:
                 raise exceptions.ValidationError(
-                        _("A session's instructor can't be an attendee"))
+                    ("A session's instructor can't be an attendee"))
